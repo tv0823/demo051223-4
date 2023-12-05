7937d7748e6def00fa43c5a7f93e2a87ed0fc69e
tv0823 <tv0823@bs.amalnet.k12.il>
2023-12-05 09:59:14 +0200

    BOOM

--- app/src/main/java/com/example/demo051223_4/MainActivity.java
+++ app/src/main/java/com/example/demo051223_4/MainActivity.java
@@ -3,12 +3,25 @@ package com.example.demo051223_4;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button btn;
+    int clicked = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void sevenboom(View view) {
+        btn = findViewById(R.id.btn);
+        clicked++;
+        if ((clicked % 7 == 0))
+            btn.setText("BOOM !");
+        else {
+            btn.setText("This is a click number:" + clicked);
+        }
     }
 }
